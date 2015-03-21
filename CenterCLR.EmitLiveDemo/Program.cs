7a95b80491b4717fa1042caa7256fea59516d89d
unknown <kouji@unicorn.ad.kekyo.net>
2015-03-21 13:27:35 +0900

    Initial commit (Before demonstration).

---
+++ CenterCLR.EmitLiveDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterCLR.EmitLiveDemo
+{
+	class Program
+	{
+		private static Action GenerateAction()
+		{
+			var assemblyName = new AssemblyName("EmitLive");
+
+			var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule("EmitLiveModule");
+
+			// public static class SampleClass { ... } のようなクラスを定義する
+			var typeBuilder = moduleBuilder.DefineType(
+				"SampleClass",
+				TypeAttributes.Public | TypeAttributes.Abstract | TypeAttributes.Sealed | TypeAttributes.Class,
+				typeof(object));
+
+			//////////////////////////////////////////////////////////
+
+			// public static void SampleMethod() { ... } のようなメソッドを定義する
+			var methodBuilder = typeBuilder.DefineMethod(
+				"SampleMethod",
+				MethodAttributes.Public | MethodAttributes.Static,
+				CallingConventions.Standard,
+				typeof (void),
+				Type.EmptyTypes);
+
+			// Console.WriteLine(string) メソッドのメソッド定義を取得する
+			var writeLineMethod = typeof(Console).GetMethod("WriteLine", new[] { typeof(string) });
+
+
+			var ilGenerator = methodBuilder.GetILGenerator();
+			ilGenerator.Emit(OpCodes.Ldstr, "Hello Center CLR!");
+			ilGenerator.Emit(OpCodes.Call, writeLineMethod);
+			ilGenerator.Emit(OpCodes.Ret);
+
+
+
+
+
+			//////////////////////////////////////////////////////////
+
+			// 使用可能な型として実体化する
+			var type = typeBuilder.CreateType();
+
+			// そこから使用可能なメソッド定義を取得する
+			var method = type.GetMethod("SampleMethod");
+
+			// メソッド定義が示すメソッドを呼び出し可能なデリゲートを生成する
+			return (Action)Delegate.CreateDelegate(typeof(Action), method);
+		}
+
+		static void Main(string[] args)
+		{
+			// デリゲート生成
+			var action = GenerateAction();
+
+			// 実行
+			action();
+		}
+	}
+}
---
+++ CenterCLR.EmitLiveDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("CenterCLR.EmitLiveDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CenterCLR.EmitLiveDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("bce4c056-a531-4240-a51d-0ee732361b5c")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
